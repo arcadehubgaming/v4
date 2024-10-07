07f577e9269692708295343da45e991cb1fded3c
chyves <82107846+notchyves@users.noreply.github.com>
2024-10-07 10:25:38 -0400

    bugfix

--- js/items/games.js
+++ js/items/games.js
@@ -1,10 +1,10 @@
 ArcadeHubItems.Games = [
     {
         "name": "1v1.LOL (Old)",
-        "url": "/games/1v1.lol/index.html"
+        "url": "games/1v1.lol/index.html"
     },
     {
         "name": "Among Us",
-        "url": "/games/among us/index.html"
+        "url": "games/among us/index.html"
     }
 ];
