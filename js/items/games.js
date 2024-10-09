3a33f73463cad1c2915db6f97522449573c3f20e
chyves <chyvescontact@gmail.com>
2024-10-08 20:12:57 -0400

    subway surfers and others

--- js/items/games.js
+++ js/items/games.js
@@ -16,12 +16,60 @@ ArcadeHubItems.Games = [
         "url": "games/Basketball Stars/index.html"
     },
     {
+        "name": "Bitlife",
+        "url": "games/bitlife/index.html"
+    },
+    {
         "name": "Candy Crush",
         "url": "games/candy crush/index.html"
     },
     {
         "name": "Cookie Clicker",
         "url": "games/Cookie Clicker/index.html"
+    },
+    {
+        "name": "Drift Hunters",
+        "url": "games/Drift Hunters/index.html"
+    },
+    {
+        "name": "Subway Surfers (Barcelona)",
+        "url": "games/subway surfers unity/barcelona.html"
+    },
+    {
+        "name": "Subway Surfers (Beijing)",
+        "url": "games/subway surfers unity/beijing.html"
+    },
+    {
+        "name": "Subway Surfers (Berlin)",
+        "url": "games/subway surfers unity/berlin.html"
+    },
+    {
+        "name": "Subway Surfers (Buenos Aires)",
+        "url": "games/subway surfers unity/buenos-aires.html"
+    },
+    {
+        "name": "Subway Surfers (Houston)",
+        "url": "games/subway surfers unity/houston.html"
+    },
+    {
+        "name": "Subway Surfers (Iceland)",
+        "url": "games/subway surfers unity/iceland.html"
+    },
+    {
+        "name": "Subway Surfers (Mexico)",
+        "url": "games/subway surfers unity/mexico.html"
+    },
+    {
+        "name": "Subway Surfers (Miami)",
+        "url": "games/subway surfers unity/miami.html"
+    },
+    {
+        "name": "Subway Surfers (San Fransisco)",
+        "url": "games/subway surfers san fransisco/index.html"
+    },
+    {
+        "name": "Subway Surfers (Winter Holiday)",
+        "url": "games/subway surfers unity/winterholiday.html"
     },
     {
         "name": "Territorial.io",
