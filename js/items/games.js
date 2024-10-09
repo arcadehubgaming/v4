b26a6e005c4dd6ee17b25e5268127ef5050c5585
chyves <chyvescontact@gmail.com>
2024-10-08 20:36:35 -0400

    fnf in the games after 3 gb upload

--- js/items/games.js
+++ js/items/games.js
@@ -32,8 +32,160 @@ ArcadeHubItems.Games = [
         "url": "games/Drift Hunters/index.html"
     },
     {
-        "name": "Friday Night Funkin",
-        "url": "games/"
+        "name": "Friday Night Funkin' (shaggymatt)",
+        "url": "games/fnf/shaggymatt"
+    },
+    {
+        "name": "Friday Night Funkin' (corruption)",
+        "url": "games/fnf/Corruption"
+    },
+    {
+        "name": "Friday Night Funkin' (Miku)",
+        "url": "games/fnf/Miku"
+    },
+    {
+        "name": "Friday Night Funkin' (vs XE)",
+        "url": "games/fnf/XE"
+    },
+    {
+        "name": "Friday Night Funkin' (ann)",
+        "url": "games/fnf/ann"
+    },
+    {
+        "name": "Friday Night Funkin' (bsides)",
+        "url": "games/fnf/bsides"
+    },
+    {
+        "name": "Friday Night Funkin' (deep sea date)",
+        "url": "games/fnf/deepseadate"
+    },
+    {
+        "name": "Friday Night Funkin' (EATEOF)",
+        "url": "games/fnf/everywhereattheendoffunk"
+    },
+    {
+        "name": "Friday Night Funkin' (FlipSide)",
+        "url": "games/fnf/flipside"
+    },
+    {
+        "name": "Friday Night Funkin' (Heck beats)",
+        "url": "games/fnf/h3llbeats"
+    },
+    {
+        "name": "Friday Night Funkin' (vs Henry Stickmin)",
+        "url": "games/fnf/henry"
+    },
+    {
+        "name": "Friday Night Funkin' (vs Hex)",
+        "url": "games/fnf/hex"
+    },
+    {
+        "name": "Friday Night Funkin' (vs kapi old)",
+        "url": "games/fnf/kapi"
+    },
+    {
+        "name": "Friday Night Funkin' (vs Matt first 2 weeks)",
+        "url": "games/fnf/matt"
+    },
+    {
+        "name": "Friday Night Funkin' (Minus)",
+        "url": "games/fnf/minus"
+    },
+    {
+        "name": "Friday Night Funkin' (vs Monika Full Week)",
+        "url": "games/fnf/monika"
+    },
+    {
+        "name": "Friday Night Funkin' (FNF Neo old)",
+        "url": "games/fnf/neo"
+    },
+    {
+        "name": "Friday Night Funkin' (Michael Jackson's Smooth Criminal over Roses)",
+        "url": "games/fnf/rosecriminal"
+    },
+    {
+        "name": "Friday Night Funkin' (Salty's Sunday Night)",
+        "url": "games/fnf/saltyssundaynight"
+    },
+    {
+        "name": "Friday Night Funkin' (vs Shaggy old)",
+        "url": "games/fnf/shaggy"
+    },
+    {
+        "name": "Friday Night Funkin' (vs Shaggy HD)",
+        "url": "games/fnf/shaggyhd"
+    },
+    {
+        "name": "Friday Night Funkin' (vs Garcello)",
+        "url": "games/fnf/smokeemoutstruggle"
+    },
+    {
+        "name": "Friday Night Funkin' (Star Catcher)",
+        "url": "games/fnf/starcatcher"
+    },
+    {
+        "name": "Friday Night Funkin' (vs Impostor 2.0)",
+        "url": "games/fnf/sussy2"
+    },
+    {
+        "name": "Friday Night Funkin' (vs Ex)",
+        "url": "games/fnf/tabi"
+    },
+    {
+        "name": "Friday Night Funkin' (vs tord)",
+        "url": "games/fnf/tord"
+    },
+    {
+        "name": "Friday Night Funkin' (Touhou)",
+        "url": "games/fnf/touhou"
+    },
+    {
+        "name": "Friday Night Funkin' (FNF Upside)",
+        "url": "games/fnf/upside"
+    },
+    {
+        "name": "Friday Night Funkin' (vs Void)",
+        "url": "games/fnf/void"
+    },
+    {
+        "name": "Friday Night Funkin' (vs Chara)",
+        "url": "games/fnf/vschara"
+    },
+    {
+        "name": "Friday Night Funkin' (Week 6)",
+        "url": "games/fnf/week6"
+    },
+    {
+        "name": "Friday Night Funkin' (Week 7)",
+        "url": "games/fnf/week7"
+    },
+    {
+        "name": "Friday Night Funkin' (vs Whitty)",
+        "url": "games/fnf/whitman"
+    },
+    {
+        "name": "Friday Night Funkin' (X Sides)",
+        "url": "games/fnf/x-sides"
+    },
+    {
+        "name": "Friday Night Funkin' (X event)",
+        "url": "games/fnf/xevent"
+    },
+    {
+        "name": "Friday Night Funkin' (vs Zardy old)",
+        "url": "games/fnf/zardy"
+    },
+    {
+        "name": "Friday Night Funkin' (Tricky FULL WEEK)",
+        "url": "games/fnf/tricky"
+    },
+    {
+        "name": "Friday Night Funkin' (Impostor V3)",
+        "url": "games/fnf/"
+    },
+    {
+        "name": "Friday Night Funkin' (Dust Tale)",
+        "url": "games/fnf/dusttale"
     },
     {
         "name": "Subway Surfers (Barcelona)",
