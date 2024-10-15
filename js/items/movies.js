b375ae81cbdb69a4084bcba3b4120e38f7d14b76
chyves <82107846+notchyves@users.noreply.github.com>
2024-10-15 12:16:26 -0400

    spiderman

--- js/items/movies.js
+++ js/items/movies.js
@@ -2,5 +2,37 @@ ArcadeHubItems.Movies = [
     {
         "name": "South Park - Bigger, Longer & Uncut",
         "url": "https://drive.google.com/file/d/1povW59rvEfsLf7xfXE3Q_9m0FITjUbUV/preview"
+    },
+    {
+        "name": "Super Mario Bros. Movie",
+        "url": "https://drive.google.com/file/d/1OsyF2LKJqjtv0b2Xd9GWOLcYkzny3VZo/preview"
+    },
+    {
+        "name": "Spider-Man: Far From Home (2019)",
+        "url": "https://drive.google.com/file/d/1YFUpSLmxb6xIlbdJXScIkRYvwYxvJ_18/preview"
+    },
+    {
+        "name": "Spider-Man: Homecoming (2017)",
+        "url": "https://drive.google.com/file/d/1H6yP8ItXHjAT69uL8CTAlvi90sPx2399/preview"
+    },
+    {
+        "name": "The Amazing Spider Man 2",
+        "url": "https://drive.google.com/file/d/1GLmG41OhRt9YfRa4r5nwvw8hCIS1Sofv/preview"
+    },
+    {
+        "name": "The Amazing Spider Man",
+        "url": "https://drive.google.com/file/d/1jom1D1Dc3eF3ZmCXVzh8lhlXrFGOBpXK/preview"
+    },
+    {
+        "name": "Spider-Man 3 (2007)",
+        "url": "https://drive.google.com/file/d/1vi0w7U65XeR-u00AQ8uS9s3C-IoiV-_1/preview"
+    },
+    {
+        "name": "Spider-Man 2 (2004)",
+        "url": "https://drive.google.com/file/d/1BJTsBEOeTbpv3fqU0ejzb44aQGrdLhyN/preview"
+    },
+    {
+        "name": "Spider-Man Full Movie",
+        "url": "https://drive.google.com/file/d/14BaJhmDRr3veoH436CKdPDp9r-j5214I/view"
     }
 ];  
