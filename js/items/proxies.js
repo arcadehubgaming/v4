2428153d294783890534d87fe2238a940d4b42bf
chyves <82107846+notchyves@users.noreply.github.com>
2024-10-16 08:37:27 -0400

    Update proxies.js

--- js/items/proxies.js
+++ js/items/proxies.js
@@ -1,6 +1,22 @@
 ArcadeHubItems.Proxies = [
     {
-        "name": "proxy",
-        "url": "https://www.google.com"
+        "name": "Light (Server #1)",
+        "url": "https://not-made-by-a-potato.meexpress.net/"
+    },
+    {
+        "name": "Light (Server #2)",
+        "url": "https://shallow.velolife.co.za/"
+    },
+    {
+        "name": "Light (Server #3)",
+        "url": "https://florida.hilleberg.tw/"
+    },
+    {
+        "name": "Equinox (Server #1)",
+        "url": "https://thebeta.duckdns.org/"
+    },
+    {
+        "name": "Rammerhead (Server #1)",
+        "url": "https://rammer.duckdns.org/"
     }
 ];
