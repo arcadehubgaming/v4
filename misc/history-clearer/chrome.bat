6912af9aa1008722af26e716fc3c3d59508c58f4
chyves <82107846+notchyves@users.noreply.github.com>
2024-11-06 15:33:01 +0000

    switch to 3kh0 assets

---
+++ misc/history-clearer/chrome.bat
@@ -0,0 +1,5 @@
+cd C:\Users\%username%\Appdata\Local\Google\Chrome\User Data\Default
+del History
+del History-journal
+rmdir Sessions /S /Q
+start chrome
---
+++ misc/history-clearer/edge.bat
@@ -0,0 +1,5 @@
+cd C:\Users\%username%\Appdata\Local\Microsoft\Edge\User Data\Default
+del History
+del History-journal
+rmdir Sessions /S /Q
+start chrome
