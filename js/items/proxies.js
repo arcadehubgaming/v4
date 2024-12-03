7aae4ab306fb3e22aeddab375ca13e8f40c84427
chyves <82107846+notchyves@users.noreply.github.com>
2024-12-03 10:03:00 -0500

    Update proxies.js

--- js/items/proxies.js
+++ js/items/proxies.js
@@ -1,13 +1,5 @@
 ArcadeHubItems.Proxies = [
     {
-        "name": "Interstellar (Server #1)",
-        "url": "https://kisei.laviewddns.com/"
-    },
-    {
-        "name": "Interstellar (Server #2)",
-        "url": "https://dartn-zako.laviewddns.com/"
-    },
-    {
         "name": "Doge Unblocker (Server #1) (Has Ads)",
         "url": "https://renrendane.laviewddns.com/"
     },
@@ -32,24 +24,8 @@ ArcadeHubItems.Proxies = [
         "url": "https://evony.laviewddns.com/"
     },
     {
-        "name": "Equinox (Server #1)",
-        "url": "https://thebeta.duckdns.org/"
-    },
-    {
         "name": "Rammerhead (Server #1)",
         "url": "https://rammer.duckdns.org/"
-    },
-    {
-        "name": "Kazwire (Server #1)",
-        "url": "https://english.hitremixes.com/"
-    },
-    {
-        "name": "Kazwire (Server #2)",
-        "url": "https://kazwire.idigo.org/"
-    },
-    {
-        "name": "Kazwire (Server #3)",
-        "url": "https://history.lodgegomantak.org/"
     },
     {
         "name": "Emerald (Server #1)",
@@ -89,10 +65,12 @@ ArcadeHubItems.Proxies = [
     },
     {
         "name": "CroxyProxy (Server #1)",
-        "url": "https://67.207.72.190/"
+        "url": "https://67.207.72.190/",
+		"noaboutblank": true
     },
     {
         "name": "CroxyProxy (Server #2)",
-        "url": "https://108.181.32.49/"
+        "url": "https://108.181.32.49/",
+		"noaboutblank": true
     }
 ];
